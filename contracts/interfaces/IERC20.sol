747cf1e89aa2451ee2fd2e506d302586a421fb21
coolcode <coolcode@live.com>
2021-07-12 23:13:20 +0800

    init Chis

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
+pragma solidity >=0.6.0;
 
 interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint value);
